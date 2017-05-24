6b1e11bb403a2c66cab6aac9af997a8b6de984b5
tc <qw805880101@qq.com>
2017-05-24 22:11:37 +0800

    tc

---
+++ pickerview/src/main/java/com/bigkoo/pickerview/listener/ItemListen.java
@@ -0,0 +1,11 @@
+package com.bigkoo.pickerview.listener;
+
+/**
+ * Created by tc on 2017/5/24.
+ */
+
+public interface ItemListen {
+    void onItemListenView_1(int index);
+    void onItemListenView_2(int index);
+    void onItemListenView_3(int index);
+}
