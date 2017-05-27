168d571fd381fdfda2a78aa27e41baf019085b66
mylovejym <644649160@qq.com>
2017-05-27 17:37:24 +0800

    wr

--- app/src/main/java/com/zhizhen/ybb/MainActivity.java
+++ app/src/main/java/com/zhizhen/ybb/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zhizhen.ybb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.home_page_layout);
+
+//        chartView = (BarChartView) findViewById(R.id.chartView);
+////        Mat mat1 = new Mat();
+//
+////        int a = ((('D')<<24)|(('O')<<16)|(('S')<<8)|('P')) * 0x9e370001;
+////        System.out.println(""+a);
+////        long l = a& 0xFFFFFFFFl;
+////        System.out.println(""+l);
+//        List<String> tagging = new ArrayList<>();
+//        List<String> xRawData = new ArrayList<>();
+//        List<Float> yRawData = new ArrayList<>();
+//        for(int i = 0 ;i<6;i++){
+//            tagging.add(i+"a");
+//            xRawData.add(i+"b");
+//            yRawData.add((float) i);
+//        }
+//
+//        chartView.setData(null,xRawData,yRawData);
     }
 
 }
